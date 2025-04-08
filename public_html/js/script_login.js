f2cfef1611b58a16ce3b6609d4a47fb15a76a779
hui-seon215 <wjatnsdl1215>
2025-04-08 16:26:38 +0900

    first upload

--- public_html/js/script_login.js
+++ public_html/js/script_login.js
@@ -1,21 +1,3 @@
-$(document).ready(function () {
-    if (!Kakao.isInitialized()) {
-        Kakao.init('45edffd2681daa9c5522b083fb28ec7d'); // SDK 초기화
-    }
-
-    $('#kakao-login-btn, #naver-login-btn ,#google-login-btn').on('click', function () {
-        kakaoLogin();
-    });
-
-    function kakaoLogin() {
-        Kakao.Auth.authorize({
-            redirectUri: 'http://wjatnsdl1215.dothome.co.kr/project05/index.html',
-            // redirectUri: 'http://localhost:5500/kakaologout.html',
-            scope: 'profile_nickname'
-        });
-    }
-});
-
 document.addEventListener("DOMContentLoaded", function () {
     const idBox = document.querySelector(".id_box");
     const passBox = document.querySelector(".pass_box");
@@ -28,7 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
             loginBtn.style.backgroundColor = "#9d9d9d"; // 기본 색상
         }
     }
-
     idBox.addEventListener("input", toggleLoginButton);
     passBox.addEventListener("input", toggleLoginButton);
 });
