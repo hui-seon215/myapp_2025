db78291715e6a88c72af86b1f1257eaf35a9ed3e
hui-seon215 <wjatnsdl1215>
2025-04-08 16:01:39 +0900

    first upload

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ let axios = require("axios");
 let app = express();
 let port = process.env.PORT || 80;
 
-//app.use(express.static("public_html"));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-});
+app.use(express.static("public_html"));
 app.listen(port, function(){
 	console.log("html 서버 시작됨")
 });
